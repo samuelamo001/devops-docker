c540ba11aa6e84682e4952f7ea01cb9b4ea0205c
samuelamo001 <samuel.amo@amalitechtraining.org>
2024-09-20 22:02:02 +0000

    modified controller

--- src/main/java/devops/advanced/devops/DemoController.java
+++ src/main/java/devops/advanced/devops/DemoController.java
@@ -4,13 +4,22 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 @RequestMapping("/api/v1")
 public class DemoController {
 
     @GetMapping("/home")
-    public String welcome() {
-        return "Welcome to the new world";
+    public List<String> welcome() {
+        List<String> welcomeMessages = new ArrayList<>();
+        welcomeMessages.add("Welcome to the new world!");
+        welcomeMessages.add("Explore the possibilities of Spring Boot.");
+        welcomeMessages.add("Discover the power of DevOps.");
+        welcomeMessages.add("Embrace the future of technology!");
+
+        return welcomeMessages;
     }
 }
 
