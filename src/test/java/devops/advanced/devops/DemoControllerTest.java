a279f6489afafb664e2869b3922b528b3a2ebf75
samuelamo001 <samuel.amo@amalitechtraining.org>
2024-09-20 22:05:09 +0000

    modified test function

--- src/test/java/devops/advanced/devops/DemoControllerTest.java
+++ src/test/java/devops/advanced/devops/DemoControllerTest.java
@@ -15,10 +15,15 @@ class DemoControllerTest {
     private MockMvc mockMvc;
 
     @Test
-    void testWelcome() throws Exception {
+    public void welcomeShouldReturnListOfMessages() throws Exception {
+
         mockMvc.perform(get("/api/v1/home"))
                 .andExpect(MockMvcResultMatchers.status().isOk())
-                .andExpect(MockMvcResultMatchers.content().string("Welcome to the new world"));
+                .andExpect(MockMvcResultMatchers.jsonPath("$").isArray())
+                .andExpect(MockMvcResultMatchers.jsonPath("$[0]").value("Welcome to the new world!"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[1]").value("Explore the possibilities of Spring Boot."))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[2]").value("Discover the power of DevOps."))
+                .andExpect(MockMvcResultMatchers.jsonPath("$[3]").value("Embrace the future of technology!"));
     }
-
 }
+
